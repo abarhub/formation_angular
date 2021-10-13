b68d6478b120dbfc7457eca6db84629ad41b7e37
abarhub <abarhub@gmail.com>
2021-10-13 11:39:53 +0200

    debut ngrx

---
+++ starwars-game/projects/game/src/app/reducers/index.ts
@@ -0,0 +1,23 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment';
+import { EnemyDto } from '../core/models/enemy';
+import { EnemyState } from '../features/enemy/store/reducers';
+import * as enemies from '../features/enemy/store/reducers';
+
+
+export interface ApplicationState {
+  enemies: EnemyState;
+}
+
+export const reducers: ActionReducerMap<ApplicationState> = {
+  enemies: enemies.reducer
+};
+
+
+export const metaReducers: MetaReducer<ApplicationState>[] = !environment.production ? [] : [];
